19037557b8eec4bbc5c19c25ddf31efe19b0cc59
Rory Preddy <rorypreddy@gmail.com>
2020-12-03 13:42:36 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-hello-azure-tf"
+    storage_account_name = "sahelloazuretf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "rg-hello-azure" {
+  name     = "rg-hello-azure"
+  location = "northcentralus"
+}
