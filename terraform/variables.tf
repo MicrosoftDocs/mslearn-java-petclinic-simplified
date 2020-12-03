d96213ea8a658b85be1f2fc1af4e1b9ce20f46f4
Rory Preddy <rorypreddy@gmail.com>
2020-12-03 13:36:43 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "spring-petclinic"
+  default = "spring-petclinicrpza"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "spring-petclinic-app"
+  default     = "spring-petclinic-apprpza"
 }
 
 variable "location" {
