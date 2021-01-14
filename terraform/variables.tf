44635e2a4029e2941eb2542b8baae49ba31b09a5
Rory Preddy <>
2021-01-14 09:51:40 +0200

    variable names

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "spring-petclinicrpza"
+  default = "sCHANGE_ME_RESOURCE_GROUP"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "spring-petclinic-apprpza"
+  default     = "CHANGE_ME_APP_NAME"
 }
 
 variable "location" {
