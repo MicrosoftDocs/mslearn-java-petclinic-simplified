2bed09bc5de84de1461bf5a549b1abb6f3ebb79d
fabi0307 <fabi0307@users.noreply.github.com>
2021-11-29 14:30:05 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "rg_exerc_terraform"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "app_exerc_terraform"
 }
 
 variable "location" {
