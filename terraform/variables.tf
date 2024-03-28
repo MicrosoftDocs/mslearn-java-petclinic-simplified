c4d347cd9851963c8472dd84586ffca0f78de27e
snickerjp <snickerjp@gmail.com>
2024-03-28 14:21:14 +0000

    変数名を更新

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "terraform-GitHub-Actions"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "snickerjp-GitHub-Actions-demo"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-  default     = "westeurope"
+  default     = "japaneast"
 }
